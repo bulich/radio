67a7516239ef6c45753454276bc3bc51eeaf66c5
Bulat <garifb@gmail.com>
2021-03-14 22:34:38 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrap">
+    <div class="player">
+      <select v-model="activeRadio" class="select" @change="changeSource">
+        <option v-for="(radio, idx) in sources" :key="idx" :value="radio.source">
+          {{radio.name}}
+        </option>
+      </select>
+      <br>
+      <audio controls ref="audio">
+        <source :src="activeRadio" type="audio/mpeg">
+      </audio>
+    </div>
+  </div>
+</template>
+
+<script>
+import sources from './sources'
+
+export default {
+  name: 'App',
+  data: () => ({
+    activeRadio: {},
+    sources: []
+  }),
+  methods: {
+    changeSource() {
+      const audio = this.$refs.audio
+      audio.load()
+      audio.play() 
+    }
+  },
+  created () {
+    this.sources = sources
+  }
+}
+</script>
+
+<style>
+  .wrap {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .select {
+    width: 340px;
+    height: 30px;
+  }
+</style>
---
+++ src/sources.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'LOFI',
+    source: 'http://hyades.shoutca.st:8043/stream'
+  },
+  {
+    name: 'LOFI BRASIL',
+    source: 'https://live.hunter.fm/lofi_high'
+  },
+  {
+    name: 'Relax FM',
+    source: 'http://ic4.101.ru:8000/stream/air/aac/64/200'
+  },
+  {
+    name: 'Reсord Chill',
+    source: 'http://air.radiorecord.ru:8102/chil_320'
+  },
+  {
+    name: 'NRJ',
+    source: 'http://pub0101.101.ru:8000/stream/air/aac/64/99'
+  },
+  {
+    name: 'Europa Plus',
+    source: 'http://ep256.hostingradio.ru:8052/europaplus256.mp3'
+  },
+  {
+    name: 'Left Coast 70s',
+    source: 'http://ice1.somafm.com/seventies-320-mp3'
+  },
+  {
+    name: 'Record 1980s',
+    source: 'http://air.radiorecord.ru:8102/1980_320'
+  },
+  {
+    name: 'Radio Chocolate',
+    source: 'http://choco.hostingradio.ru:10010/fm'
+  },
+  {
+    name: 'Maximum',
+    source: 'https://maximum.hostingradio.ru/maximum96.aacp'
+  },
+  {
+    name: 'ULTRA',
+    source: 'http://nashe1.hostingradio.ru/ultra-256'
+  },
+]
